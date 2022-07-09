e920423c54245a5ae78ad7e7c3e67d224164c010
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-07-08 17:00:07 -0700

    Basic rendering of bars in SVG in place
    Need bar size and positioning to relate to the data next

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
 function requestData(url, backup) {
   const xhr = new XMLHttpRequest();
-  xhr.addEventListener('loadend', dataLoaded);
-  xhr.addEventListener('error', transferFailed);
+  xhr.addEventListener('loadend', (e) => dataLoaded.call(xhr, e, backup));
+  xhr.addEventListener('error', (e) => transferFailed(e));
   xhr.open('GET', url);
   xhr.send();
 }
@@ -15,7 +15,10 @@ function dataLoaded(e, backup) {
     requestData('data.json', true);
   } else if (this.status !== 200) {
     // If both attempts to load data have failed, display error message
-    transferFailed();
+    transferFailed(
+      e,
+      'Error when trying to load data from API and file, please try again.'
+    );
   } else {
     // Data loaded, render graph
     renderGraph(this.responseText);
@@ -23,12 +26,73 @@ function dataLoaded(e, backup) {
 }
 
 // Generate an error message where graph should display
-function transferFailed(e) {
-  console.log('Error occured when trying to load graph data');
+function transferFailed(
+  e,
+  message = 'An error occured when trying to load data'
+) {
+  const graphContainer = d3.select('#graph-container');
+  graphContainer.text(message);
+  console.error(message);
 }
 
 function renderGraph(rawData) {
-  console.log(rawData);
+  const graphContainer = d3.select('#graph-container');
+  graphContainer.text(''); // Remove 'Loading...' message
+
+  let dataObj;
+  try {
+    dataObj = JSON.parse(rawData);
+  } catch (err) {
+    transferFailed(
+      null,
+      'Error occured when trying to parse JSON data, please try again.'
+    );
+    return;
+  }
+
+  graphContainer.append('h1').text('US Quarterly GDP 1947-2015');
+
+  console.log(graphContainer.node().getBoundingClientRect()); // Get element width and height
+
+  const w = 1000;
+  const h = 700;
+  const padding = 30;
+
+  const graphSVG = graphContainer
+    .append('svg')
+    .attr('class', 'graph')
+    .attr('width', w)
+    .attr('height', h);
+
+  // Create scales for x and y axes of graph:
+  const gdpData = dataObj.data;
+  const gdpMax = Math.ceil(d3.max(gdpData, (d) => d[1]) / 1000) * 1000;
+
+  const yearMin = d3.min(gdpData, (d) => parseInt(d[0].slice(0, 4)));
+  const yearMax = d3.max(gdpData, (d) => parseInt(d[0].slice(0, 4))) + 1;
+
+  const xscale = d3
+    .scaleLinear()
+    .domain([yearMin, yearMax])
+    .range([padding, w - padding]);
+  const yscale = d3
+    .scaleLinear()
+    .domain([0, gdpMax])
+    .range([h - padding, padding]);
+
+  // Add data bars to the chart
+  graphSVG
+    .selectAll('rect')
+    .data(gdpData)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('x', (d, i) => i * 10)
+    .attr('width', 10)
+    .attr('class', 'graph-bar')
+    .attr('height', 100)
+    .attr('fill', 'black')
+    .attr('stroke', 'white');
 }
 
 // Request graph data after DOM loads
